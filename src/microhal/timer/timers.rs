b7737101dddbbd77e8e134ea259ecc1956fdc160
Andrey Sverdlichenko <blaze@rusty.zone>
2024-06-05 00:39:01 -0400

    Generate PWM via macros

--- src/microhal/timer/timers.rs
+++ src/microhal/timer/timers.rs
@@ -79,6 +79,21 @@ macro_rules! general_purpose_timer {
 
             basic_timer_impl!($TIM, $REG);
         }
+
+        impl Pwm<$TIM> {
+            pub fn bind_pin<PIN>(&self, pin: PIN) -> PwmPin<$TIM, PIN::Channel>
+            where
+                PIN: TimerPin<$TIM>,
+            {
+                // Set channel mode to PWM1
+                pin.set_pwm_mode(&self.timer);
+
+                PwmPin {
+                    timer: &self.timer,
+                    channel: PhantomData,
+                }
+            }
+        }
     };
 }
 
@@ -99,11 +114,8 @@ general_purpose_timer!(TIM2, u32);
 general_purpose_timer!(TIM3, u16);
 
 pub struct Channel1;
-
 pub struct Channel2;
-
 pub struct Channel3;
-
 pub struct Channel4;
 
 pub trait TimerPin<TIM> {
@@ -120,108 +132,45 @@ impl<TIM, CH> embedded_hal::pwm::ErrorType for PwmPin<'_, TIM, CH> {
     type Error = core::convert::Infallible;
 }
 
-impl embedded_hal::pwm::SetDutyCycle for PwmPin<'_, TIM3, Channel1> {
-    fn max_duty_cycle(&self) -> u16 {
-        self.timer.arr.read().arr_l().bits()
-    }
+macro_rules! pwm {
+    ($TIM:ident, [$($CH:ident => $CCR:ident, $CCR_L:ident, $ENBIT:ident, $CCMR:ident, $MODE:ident, $POLBIT:ident,
+        [$(($PIN:ident,$AF:literal),)+],)+]) => {
+        $(
+            // Implement SetDutyCycle for each channel of this timer.
+            impl embedded_hal::pwm::SetDutyCycle for PwmPin<'_, $TIM, $CH> {
+                fn max_duty_cycle(&self) -> u16 {
+                    self.timer.arr.read().arr_l().bits()
+                }
 
-    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
-        self.timer.ccr1.write(|w| w.ccr1_l().variant(duty));
-        Ok(())
-    }
+                fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
+                    self.timer.$CCR.write(|w| w.$CCR_L().variant(duty));
+                    Ok(())
+                }
+            }
+
+            // Implement TimerPin for each pin of each channel.
+            $(
+                impl TimerPin<$TIM> for $PIN<Alternate<$AF>> {
+                    type Channel = $CH;
+
+                    fn set_pwm_mode(&self, tim: &$TIM) {
+                        // Disable channel
+                        tim.ccer.modify(|_, w| w.$ENBIT().clear_bit());
+                        // Set PWM mode and enable preload
+                        tim.$CCMR().modify(|_, w| w.$MODE().pwm_mode1().$POLBIT().set_bit());
+                        // Enable channel
+                        tim.ccer.modify(|_, w| w.$ENBIT().set_bit());
+                    }
+                }
+            )+
+        )+
+    };
 }
 
-impl embedded_hal::pwm::SetDutyCycle for PwmPin<'_, TIM3, Channel2> {
-    fn max_duty_cycle(&self) -> u16 {
-        self.timer.arr.read().arr_l().bits()
-    }
-
-    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
-        self.timer.ccr2.write(|w| w.ccr2_l().variant(duty));
-        Ok(())
-    }
-}
-
-impl embedded_hal::pwm::SetDutyCycle for PwmPin<'_, TIM3, Channel3> {
-    fn max_duty_cycle(&self) -> u16 {
-        self.timer.arr.read().arr_l().bits()
-    }
-
-    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
-        self.timer.ccr3.write(|w| w.ccr3_l().variant(duty));
-        Ok(())
-    }
-}
-
-impl embedded_hal::pwm::SetDutyCycle for PwmPin<'_, TIM3, Channel4> {
-    fn max_duty_cycle(&self) -> u16 {
-        self.timer.arr.read().arr_l().bits()
-    }
-
-    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
-        self.timer.ccr4.write(|w| w.ccr4_l().variant(duty));
-        Ok(())
-    }
-}
-
-impl TimerPin<TIM3> for PB0<Alternate<1>> {
-    type Channel = Channel3;
-
-    fn set_pwm_mode(&self, tim: &TIM3) {
-        // Disable channel
-        tim.ccer.modify(|_, w| w.cc3e().clear_bit());
-        // Set PWM mode and preload
-        tim.ccmr2_output()
-            .modify(|_, w| w.oc3m().pwm_mode1().oc3pe().set_bit());
-        // Enable channel
-        tim.ccer.modify(|_, w| w.cc3e().set_bit());
-    }
-}
-
-impl TimerPin<TIM3> for PB1<Alternate<1>> {
-    type Channel = Channel4;
-
-    fn set_pwm_mode(&self, tim: &TIM3) {
-        tim.ccer.modify(|_, w| w.cc4e().clear_bit());
-        tim.ccmr2_output()
-            .modify(|_, w| w.oc4m().pwm_mode1().oc4pe().set_bit());
-        tim.ccer.modify(|_, w| w.cc4e().set_bit());
-    }
-}
-
-impl TimerPin<TIM3> for PB4<Alternate<1>> {
-    type Channel = Channel1;
-
-    fn set_pwm_mode(&self, tim: &TIM3) {
-        tim.ccer.modify(|_, w| w.cc1e().clear_bit());
-        tim.ccmr1_output()
-            .modify(|_, w| w.oc1m().pwm_mode1().oc1pe().set_bit());
-        tim.ccer.modify(|_, w| w.cc1e().set_bit());
-    }
-}
-
-impl TimerPin<TIM3> for PB5<Alternate<1>> {
-    type Channel = Channel2;
-
-    fn set_pwm_mode(&self, tim: &TIM3) {
-        tim.ccer.modify(|_, w| w.cc2e().clear_bit());
-        tim.ccmr1_output()
-            .modify(|_, w| w.oc2m().pwm_mode1().oc2pe().set_bit());
-        tim.ccer.modify(|_, w| w.cc2e().set_bit());
-    }
-}
-
-impl Pwm<TIM3> {
-    pub fn bind_pin<PIN>(&self, pin: PIN) -> PwmPin<TIM3, PIN::Channel>
-    where
-        PIN: TimerPin<TIM3>,
-    {
-        // Set channel mode to PWM1
-        pin.set_pwm_mode(&self.timer);
-
-        PwmPin {
-            timer: &self.timer,
-            channel: PhantomData,
-        }
-    }
-}
+pwm!(TIM3, [
+        Channel1 => ccr1, ccr1_l, cc1e, ccmr1_output, oc1m, oc1pe, [(PB4, 1),],
+        Channel2 => ccr2, ccr2_l, cc2e, ccmr1_output, oc2m, oc2pe, [(PB5, 1),],
+        Channel3 => ccr3, ccr3_l, cc3e, ccmr2_output, oc3m, oc3pe, [(PB0, 1),],
+        Channel4 => ccr4, ccr4_l, cc4e, ccmr2_output, oc4m, oc4pe, [(PB1, 1),],
+    ]
+);
